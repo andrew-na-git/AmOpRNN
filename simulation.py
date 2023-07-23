11ebcc6d047f8fc7a4609a015e4e7d898a5e3265
Andrew Na <Andrew.na92@gmail.com>
2023-07-23 13:08:52 -0400

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -72,17 +72,13 @@ def train_rnn(rnn, optimizer, loader):
                 p_star[q] = math.exp(-r*dt)*u[t]
                 d_star[q] = math.exp(-r*dt)*z[t]*(s[t]/s[q])
             if q > 1:
-                _,stop_idx = torch.max(u[:q+1].mean(1),0)
                 for k in range(eff_d):
                     delta_tau = q - stop_idx[k]
                     p_star[q,:,k] = math.exp(-r*delta_tau*dt)*u[int(stop_idx[k]),:,k]
                     d_star[q,:,k] = math.exp(-r*delta_tau*dt)*(z[int(stop_idx[k]),:,k]*(s[int(stop_idx[k]),:,k]/s[q,:,k]))
             
-                disc_v[t] = math.exp(-r*(q-1)*dt)*p_star[1]
-                disc_d[t] = math.exp(-r*(q-1)*dt)*d_star[1]*(s[1]/s[q])
-
-        # print(p_star.mean(1), d_star.mean(1))
-        # input()
+                disc_v[t] = math.exp(-r*dt)*p_star[t]
+                disc_d[t] = math.exp(-r*dt)*d_star[t]*(s[t]/s[q])
         
         for k in range(eff_d):
             in_p = torch.cat((s[1:,:,k].unsqueeze(-1),x[1:,:,k].unsqueeze(-1)),dim=-1) # (seq_len, batch_size, num_features/inputs)
@@ -93,7 +89,6 @@ def train_rnn(rnn, optimizer, loader):
             hd = delta_rnn.init_hidden(z[1,:,k].unsqueeze(-1))
             dpds[1:,:,k] = delta_rnn(in_d,hd,disc_d[:,:,k])
 
-        print(p[-1].mean(0), dpds[-1].mean(0))
         price_opt.zero_grad()
         delta_opt.zero_grad()
         loss_p, loss_d = loss_sde(p,p_star,dpds,d_star)
@@ -162,9 +157,6 @@ def predict_rnn(rnn, loader):
         value[0] = u[0]
         delta[0] = z[0]
 
-        cp[0] = u[0]
-        cp_delta[0] = z[0]
-
         disc_v[0] = u[0]
         disc_d[0] = z[0]
 
@@ -176,13 +168,8 @@ def predict_rnn(rnn, loader):
 
         for t in range(seq_len):
             q = t+1
-
-            if q == 1:
-                cp[q] = math.exp(-r*dt)*u[t]
-                cp_delta[q] = math.exp(-r*dt)*z[t]*(s[t]/s[q])
-            if q > 1:
-                disc_v[t] = math.exp(-r*(q-1)*dt)*cp[1]
-                disc_d[t] = math.exp(-r*(q-1)*dt)*cp_delta[1]*(s[1]/s[q])
+            disc_v[t] = math.exp(-r*dt)*u[t]
+            disc_d[t] = math.exp(-r*dt)*z[t]*(s[t]/s[q])
 
         for k in range(eff_d):
             in_p = torch.cat((s[1:,:,k].unsqueeze(-1),x[1:,:,k].unsqueeze(-1)), dim=-1) # (seq_len, batch_size, num_features/inputs)
